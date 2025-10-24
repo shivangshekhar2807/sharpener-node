77c1ff0811ce8ee0c15d72b6dc38678b5d7b655c
shivangshekhar2807 <shivangshekhar302@gmail.com>
2025-10-24 22:35:07 +0530

    Express Server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hi")
+
+const express = require("express");
+const app = express();
+
+app.listen(8888, () => {
+    console.log("server started and listening on port 8888")
+})
+
+
+app.use("/test", (req, res) => {
+    res.send("<h1>hello world</h1>")
+})
