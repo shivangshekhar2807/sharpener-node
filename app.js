be6815ec8680a6fa9d03517a3b4782c089c9253a
shivangshekhar2807 <shivangshekhar302@gmail.com>
2025-10-24 22:51:42 +0530

    Unknown route and wildcard route

--- app.js
+++ app.js
@@ -14,3 +14,15 @@ app.use((req, res, next) => {
 app.use("/test", (req, res) => {
     res.send("<h1>hello world</h1>")
 })
+
+app.use("/products", (req, res) => {
+  res.send("products");
+});
+
+app.use("/categories", (req, res) => {
+  res.send("categories");
+});
+
+app.use((req, res) => {
+  res.status(404).send("<h1>404 - Page Not Found</h1>");
+});
